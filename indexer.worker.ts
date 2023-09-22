ccd6425a63e9007aa258de1aa9cceb2ef6b0e171
Fevol <vernieuwefelix@gmail.com>
2023-09-23 01:24:06 +0200

    fix: resolve import issue

--- indexer.worker.ts
+++ indexer.worker.ts
@@ -1,5 +1,5 @@
 // Modify this file to fit your use-case
-import { getNodesInText } from '../editor/base/edit-util';
+import { getNodesInText } from '../editor/base';
 
 self.onmessage = async (event) => {
 	const files = await Promise.all(event.data.map(async (file: string) => {
